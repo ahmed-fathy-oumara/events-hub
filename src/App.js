c1289a4140e9dea52c5dfcdbb2dbdd3ec0673661
Ahmed Fathy <ahmedfathyoumara@gmail.com>
2019-07-09 21:02:20 +0200

    Info Section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './resources/styles.css';
 //Components
 import Header from './components/header_footer/Header'
 import Featured from './components/featured';
+import Info from './components/info';
 
 function App() {
   return (
     <div className="App" style={{ height:"1500px", background:'cornflowerblue' }}>
       <Header />
       <Featured />
+      <Info />
     </div>
   );
 }
